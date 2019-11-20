253e8bf3c9af7750c06ad198c01ce85c5553969a
adyasha <adymohan@in.ibm.com>
2019-11-20 17:33:00 +0530

    change the URL

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/07-feedback';
+const apiUrl = 'https://57537c97.eu-gb.apiconnect.appdomain.cloud/01-feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
